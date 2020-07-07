2a80dfbca5fabe1160dcc693f36ac2886c50ef34
Ridho Hafidz <mridhohafidz12@gmail.com>
2020-07-07 13:14:02 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ngoprek-Nolsatu
---
+++ pendekar.sql
@@ -0,0 +1,11 @@
+ 
+CREATE DATABASE IF NOT EXISTS pendekar;
+USE pendekar;
+
+CREATE TABLE favorite_colors (
+  name VARCHAR(20),
+  color VARCHAR(10)
+);
+
+INSERT INTO favorite_colors  (name, color) VALUES ('Wiro Sableng', 'White'), ('Prabu Siliwangi', 'blue');
+
